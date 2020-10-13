1651218ddd0b1178a5966ee44f4da89686fb7284
JuMpYggdrasil <70381321+JuMpYggdrasil@users.noreply.github.com>
2020-10-13 13:43:33 +0700

    Create Indoor_Positioning.ino

---
+++ AP/AP.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+
+WiFiServer server(80);
+
+String line;
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("ESP_01n1");
+
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client)
+    return;
+
+  Serial.println("New client");
+  while (client.connected()) {
+    if (client.available()) {
+      char c = client.read();
+      if (c == '\r') {
+        Serial.println(line);
+        line = "";
+        break;
+      } else if (c == '\n') {
+        // Pass {new line}
+      } else {
+        line += c; 
+      }
+    }
+  }
+
+  delay(1);
+  client.stop();
+  Serial.println("Client disconnect");
+}
